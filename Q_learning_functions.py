0949e33a50536da7cd13ad0039f47b90f0cff78a
Sudharsan Murugesan <Sudharsan.muru@gmail.com>
2026-03-11 00:15:30 +0530

    added bellman's_update

--- Q_learning_functions.py
+++ Q_learning_functions.py
@@ -6,3 +6,23 @@ def softmax(list_values, T):
     probabilities = [round(math.exp(i/T)/sum_of_exp_values,3) for i in list_values]
     return probabilities
 
+def state_updater(state, action):
+    i, j= state
+    if action == 0:
+        return (i, j+1)
+    elif action == 1:
+        return (i, j-1)
+    elif action == 2:
+        return (i+1, j)
+    else:
+        return (i-1,j)
+
+def bellmans_update(rewards, Q_values, state, action, gamma, alpha):
+    new_state = state_updater(state,action)
+    if new_state not in rewards.keys():
+        reward = -5
+    else:
+        reward = rewards[new_state]
+    target = reward + gamma*max(Q_values[new_state])
+    error = target - Q_values[state][action]
+    Q_values[state][action] = Q_values[state][action]+alpha*error
